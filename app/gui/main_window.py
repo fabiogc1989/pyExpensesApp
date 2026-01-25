eaef4fce3e2f32552fa0d25ad28a4c25c5066d9d
Fabio Coimbra <fabiogc1989@gmail.com>
2026-01-25 16:58:32 +0000

    improve a litle of main window

--- app/gui/main_window.py
+++ app/gui/main_window.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import ttk, Menu
+from tkinter import ttk
 from .screens import StartScreen, BankAccountScreen
 
 
@@ -7,42 +7,39 @@ class MainWindow(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title('pyExpenses')
-        self.geometry('800x600')
+        # self.geometry('800x600')
 
         self._setup_widgets()
 
         # Container for screens
         self.container = tk.Frame(self)
-        self.container.pack(fill='both', expand=True)
+        self.container.pack(fill="both", expand=True)
 
         # Initialize screens
-        self.screens = {}
-        for ScreenClass in (StartScreen, BankAccountScreen):
-            frame = ScreenClass(self.container)
-            self.screens[ScreenClass] = frame
-            frame.grid(row=0, column=0, sticky='nsew')
-        
+        self._current_screen = None        
         self.show_screen(StartScreen)
         
     def _setup_widgets(self):
         # Create menu bar
-        menu_bar = Menu(master=self)
+        menu_bar = tk.Menu(master=self)
         self.config(menu=menu_bar)
 
         # File menu
-        file_menu = Menu(menu_bar, tearoff=0)
+        file_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label='File', menu=file_menu)
 
         # Bank Account Menu
-        bank_account_menu = Menu(menu_bar, tearoff=0)
+        bank_account_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Bank Account", menu=bank_account_menu)
         bank_account_menu.add_command(label="View Accounts", command=lambda: self.show_screen(BankAccountScreen))
 
         # Help menu
-        help_menu = Menu(menu_bar, tearoff=0)
+        help_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Help", menu=help_menu)
-        help_menu.add_command(label="About", command=lambda: print("Tkinter App v1.0"))
+        help_menu.add_command(label="About", command=lambda: self.show_screen(StartScreen))
     
     def show_screen(self, screen_class):
-        frame = self.screens[screen_class]
-        frame.tkraise() # Bring the selected screen to the front
+        if self._current_screen is not None:
+            self._current_screen.destroy()
+        self._current_screen = screen_class(self.container)
+        self._current_screen.tkraise() # Bring the selected screen to the front
