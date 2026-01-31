561a30e56d0e8027c9569fe50f77505e55b85c9f
Fabio Coimbra <fabiogc1989@gmail.com>
2026-01-31 14:29:35 +0000

    method __setup_widgets

--- app/gui/screens.py
+++ app/gui/screens.py
@@ -1,13 +1,15 @@
-import tkinter as tk
-from tkinter import ttk
 import app.gui.widgets as widgets
+import tkinter as tk
 
 
 class StartScreen(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(fill="both", expand=True)
-        label = ttk.Label(self, text="Welcome to pyExpenses!", font=("Arial", 18))
+        self.__setup_widgets()
+    
+    def __setup_widgets(self):
+        label = tk.Label(self, text="Welcome to pyExpenses!", font=("Arial", 18))
         label.pack(pady=20)
 
 
@@ -15,11 +17,20 @@ class BankAccountScreen(tk.Frame):
     def __init__(self, master):
         super().__init__(master)
         self.pack(fill="both", expand=True)
+        self.__setup_widgets()
 
+    def __setup_widgets(self):
+        search_frame = tk.Frame(master=self)
+        iban_search_entry = tk.Entry(master=search_frame)
+        iban_search_entry.grid(column=0, row=0)
+        search_button = tk.Button(master=search_frame, text="Search")
+        search_button.grid(column=1, row=0)
+        search_frame.pack(anchor="ne")
+        
         # Define columns
         columns = ("id","iban",)
-
         table = widgets.ScrollableTreeView(self, columns=columns, show="headings")
+        table.pack(side=tk.LEFT, fill="both", expand=True)
 
         # Configure the headers (what appears on top)
         for column in columns:
@@ -30,3 +41,4 @@ class BankAccountScreen(tk.Frame):
         data = [(x, "PT50" + str(x) * 28) for x in range(1, 501)]
         for item in data:
             table.tree_view.insert(parent="",index=tk.END,values=item)
+        
