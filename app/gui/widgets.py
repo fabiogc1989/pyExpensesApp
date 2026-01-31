2d8dab462c681d2edf23ae55332feddfe90d9313
Fabio Coimbra <fabiogc1989@gmail.com>
2026-01-31 14:29:11 +0000

    oop attributes as private

--- app/gui/widgets.py
+++ app/gui/widgets.py
@@ -6,18 +6,19 @@ class ScrollableTreeView(tk.Frame):
     def __init__(self, master, columns, *args, **kwargs):
         super().__init__(master)
 
-        self._treeview = ttk.Treeview(master, columns=columns, *args, **kwargs)
+        self.__treeview = ttk.Treeview(master, columns=columns, *args, **kwargs)
 
         # Create scrollbars
-        self.vertical_scrollbar = ttk.Scrollbar(master=self._treeview, orient=tk.VERTICAL, command=self._treeview.yview)
-        self.horizontal_scrollbar = ttk.Scrollbar(master=self._treeview, orient=tk.HORIZONTAL, command=self._treeview.xview)
+        self.__vertical_scrollbar = ttk.Scrollbar(master=self.__treeview, orient=tk.VERTICAL, command=self.__treeview.yview)
+        self.__horizontal_scrollbar = ttk.Scrollbar(master=self.__treeview, orient=tk.HORIZONTAL, command=self.__treeview.xview)
         # Configure the Treeview to use the scrollbars
-        self._treeview.configure(yscrollcommand=self.vertical_scrollbar.set, xscrollcommand=self.horizontal_scrollbar.set)
-        # Pack the scrollbars
-        self.horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
-        self.vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
-        self._treeview.pack(side=tk.LEFT, fill="both", expand=True)
+        self.__treeview.configure(yscrollcommand=self.__vertical_scrollbar.set, xscrollcommand=self.__horizontal_scrollbar.set)
     
     @property
     def tree_view(self):
-        return self._treeview
+        return self.__treeview
+    
+    def pack(self, side=tk.LEFT, fill="both",expand=False, *args, **kwargs):
+        self.__horizontal_scrollbar.pack(side=tk.BOTTOM, fill=tk.X)
+        self.__vertical_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+        self.__treeview.pack(side=side, fill=fill, expand=expand, *args, **kwargs)
