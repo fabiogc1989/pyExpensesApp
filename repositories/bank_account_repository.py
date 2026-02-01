4aea72d5d94389dd2e1de7f5f98a7fba60551bd1
Fabio Coimbra <fabiogc1989@gmail.com>
2026-02-01 14:42:53 +0000

    banck account repo

--- repositories/bank_account_repository.py
+++ repositories/bank_account_repository.py
@@ -1,18 +1,47 @@
+from core.database import SessionLocal
 from models.bank_account import BankAccount
 from .base_repository import BaseRepository
 
 class BankAccountRepository(BaseRepository[BankAccount]):
     def get_all(self) -> list[BankAccount]|iter[BankAccount]:
-        ...
+        try:
+            with SessionLocal() as session:
+                entities = session.query(BankAccount).all()
+            return entities
+        except:
+            return []
 
     def get(self, id: int) -> BankAccount:
-        ...
+        try:
+            with SessionLocal() as session:
+                entity = session.get(BankAccount, id)
+            return entity
+        except:
+            return None
 
     def delete(self, entity: BankAccount) -> bool:
-        ...
+        try:
+            with SessionLocal() as session:
+                session.delete(entity)
+                session.commit()
+            return True
+        except:
+            return False
 
     def update(self, entity: BankAccount) -> bool:
-        ...
+        try:
+            with SessionLocal() as session:
+                session.merge(entity)
+                session.commit()
+            return True
+        except:
+            return False
     
     def insert(self, entity: BankAccount) -> bool:
-        ...
+        try:
+            with SessionLocal() as session:
+                session.add(entity)
+                session.commit()
+            return True
+        except:
+            return False
