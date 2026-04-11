181e9c410f9063ab770c09c9bccd83f0f30dad2a
Fabio Coimbra <fabiogc1989@gmail.com>
2026-04-11 14:48:25 +0100

    add transaction view repository without calling the base class

--- src/gui/screen/view_transactions_screen.py
+++ src/gui/screen/view_transactions_screen.py
@@ -1,13 +1,23 @@
 import tkinter as tk 
 from tkinter import ttk
 
+from src.core.di import Inject
 from src.model.design_pattern.creational_pattern.scrollable_tree_view_builder import ScrollableTreeViewBuilder, ScrollableTreeViewDirector
+from src.repository.transaction_view_repository import TransactionViewRepository
 
 
 class ViewTransactionsScreen(ttk.Frame):
+    repository = Inject(TransactionViewRepository)
+
     def __init__(self, master):
         super().__init__(master)
         self.pack(fill="both", expand=True)
 
         scrollableTreeViewDirrector = ScrollableTreeViewDirector(ScrollableTreeViewBuilder(master=self, columns=('id', 'description', 'amount', 'date', 'type', 'bank_account_id')))
         self.table = scrollableTreeViewDirrector.build_scrollable_tree_view(show='headings', selectmode='browse', fill='both', side=tk.LEFT, expand=True)
+
+        # Insert data
+        data = self.repository.get_all()
+        for item in data:
+            self.table.tree_view.insert(parent="",index=tk.END, iid=item.unique_row_id, values=(item.id, item.description, item.amount, item.date, item.type, item.bank_account_id))
+        
---
+++ src/repository/transaction_view_repository.py
@@ -0,0 +1,24 @@
+from src.core.database import SessionLocal, TransactionView
+from src.exception.repository_exception import RepositoryException
+from src.model.database_schema import TransactionViewSchema
+from src.core.di import ioc
+
+@ioc.register
+class TransactionViewRepository:
+    def get_all(self) -> list[TransactionViewSchema]|iter[TransactionViewSchema]:
+        try:
+            with SessionLocal() as session:
+                entities = session.query(TransactionView).all()
+            return [TransactionViewSchema.model_validate(entity) for entity in entities]
+        except Exception as e:
+            raise RepositoryException(f"Error fetching all transaction views: {str(e)}")
+    
+    def get(self, id: int) -> TransactionViewSchema:
+        try:
+            with SessionLocal() as session:
+                entity = session.get(TransactionView, id)
+            if entity is None:
+                raise RepositoryException(f"Transaction view with ID {id} not found")
+            return TransactionViewSchema.model_validate(entity)
+        except Exception as e:
+            raise RepositoryException(f"Error fetching transaction view with ID {id}: {str(e)}")
