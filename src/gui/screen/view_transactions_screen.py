c518922922fec794b42fa50c5175ddc84a46e600
Fabio Coimbra <fabiogc1989@gmail.com>
2026-04-19 15:56:47 +0100

    add search model

--- src/gui/screen/view_transactions_screen.py
+++ src/gui/screen/view_transactions_screen.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 
 from src.core.di import Inject
 from src.model.design_pattern.creational_pattern.scrollable_tree_view_builder import ScrollableTreeViewBuilder, ScrollableTreeViewDirector
+from src.model.transaction_search import TransactionSearch
 from src.repository.transaction_view_repository import TransactionViewRepository
 from src.service.transaction_service import TransactionService
 
@@ -18,7 +19,8 @@ class ViewTransactionsScreen(ttk.Frame):
         self.table = scrollableTreeViewDirrector.build_standard_tree_view()
 
         # Insert data
-        data = self.__service.search_transactions()
+        search_model = TransactionSearch()
+        data = self.__service.search_transactions(search_model)
         for item in data:
             self.table.tree_view.insert(parent="",index=tk.END, iid=item.row_number, values=(item.transaction_id, item.description, item.amount, item.date, item.type.name, item.iban))
         
