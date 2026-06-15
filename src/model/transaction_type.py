96e507bdb85eb5a39c1f10379877bf9cd635bd85
Fabio Coimbra <fabiogc1989@gmail.com>
2026-06-15 13:39:25 +0100

    clean the file

--- src/model/transaction_type.py
+++ src/model/transaction_type.py
@@ -5,10 +5,3 @@ class TransactionType(Enum):
     ALL = None
     DEBIT = 0
     CREDIT = 1
-
-
-TransactionTypeValues = {
-    'DEBIT': TransactionType.DEBIT,
-    'CREDIT': TransactionType.CREDIT,
-    'ALL': TransactionType.ALL
-}
